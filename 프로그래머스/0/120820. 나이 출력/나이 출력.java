037eeae6b4f7658ff779add85215cc70259d3b50
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-04-24 14:10:53 +0900

    [level 0] Title: 나이 출력, Time: 0.02 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.java
@@ -0,0 +1,7 @@
+class Solution {
+    private static final int CURRENT_YEAR = 2022;
+    public int solution(int age) {
+        int answer = CURRENT_YEAR - age + 1;
+        return answer;
+    }
+}
