28ff315db315036f55e490d18b6415e58c300a46
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-04-24 17:06:51 +0900

    [level 0] Title: 양꼬치, Time: 0.03 ms, Memory: 77.3 MB -BaekjoonHub

--- 프로그래머스/0/120830. 양꼬치/README.md
+++ 프로그래머스/0/120830. 양꼬치/README.md
@@ -1,10 +1,10 @@
 # [level 0] 양꼬치 - 120830 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120830?language=java) 
 
 ### 성능 요약
 
-메모리: 82.9 MB, 시간: 0.02 ms
+메모리: 77.3 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 04월 24일 16:58:24
+2026년 04월 24일 17:06:47
 
 ### 문제 설명
 
--- 프로그래머스/0/120830. 양꼬치/양꼬치.java
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -2,13 +2,12 @@ class Solution {
     private static final int FOOD_PRICE = 12000;
     private static final int DRINK_PRICE = 2000;
     public int solution(int n, int k) {
-        int result = 0;
-        result = (n*FOOD_PRICE) + (k*DRINK_PRICE) - calcDiscount(n);
+        int result = n*FOOD_PRICE + DRINK_PRICE * (k - calcFreeDrink(n));
         
         return result;
     }
     
-    public int calcDiscount(int foodCount){
-        return (foodCount/10)*DRINK_PRICE;
+    private int calcFreeDrink(int foodCount){
+        return (foodCount/10);
     }
 }
