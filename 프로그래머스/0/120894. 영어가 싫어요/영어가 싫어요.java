c2dea310fa827aa9165dd78af8282bdd58b818c7
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-04-28 16:26:36 +0900

    [level 0] Title: 영어가 싫어요, Time: 0.15 ms, Memory: 84.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120894. 영어가 싫어요/영어가 싫어요.java
@@ -0,0 +1,10 @@
+class Solution {
+    public long solution(String numbers) {
+        String[] numArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+        
+        for(int i = 0; i < numArr.length; i++){
+            numbers = numbers.replace(numArr[i], String.valueOf(i));
+        }
+        return Long.parseLong(numbers);
+    }
+}
