6a3009953bb7aee2bee1e0c3dbfd2534600e1365
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-05-06 00:05:43 +0900

    [level 0] Title: 치킨 쿠폰, Time: 0.02 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
+++ 프로그래머스/0/120884. 치킨 쿠폰/치킨 쿠폰.java
@@ -3,13 +3,8 @@ class Solution {
         int remainingCoupon = chicken;
         int serviceChicken = 0;
         while(remainingCoupon >= 10){
-            System.out.println("전체 쿠폰 : " + remainingCoupon);
             serviceChicken += remainingCoupon/10;
-            System.out.println("치킨 누적 : " + serviceChicken);
             remainingCoupon = remainingCoupon/10 + remainingCoupon%10;
-            System.out.println("잔여 쿠폰 : " + remainingCoupon);
-            
-            System.out.println("-- 회차 종료 --");
         }
         return serviceChicken;
     }
