3c5fe34fef45c34a5595c03ad230fb04ffdcfa39
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-05-08 22:38:24 +0900

    [level 1] Title: 과일 장수, Time: 35.96 ms, Memory: 122 MB -BaekjoonHub

---
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int result = 0;
+        int box = score.length / m; 
+        Arrays.sort(score);
+        
+        int count = 0;
+        for(int i=score.length; i > 0; i--){
+            count++;
+            if(count == m){
+                count = 0;
+                result += score[i-1] * m;
+            }
+        }
+        
+        return result;
+    }
+}
