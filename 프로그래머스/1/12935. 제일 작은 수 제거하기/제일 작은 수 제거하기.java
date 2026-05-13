5b54d1a6ba5f4bf7a70bb0adfe3e0d7755453a83
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-05-13 23:02:14 +0900

    [level 1] Title: 제일 작은 수 제거하기, Time: 1.28 ms, Memory: 72.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12935. 제일 작은 수 제거하기/제일 작은 수 제거하기.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] solution(int[] arr) {
+        if(arr.length == 1){
+            return new int[] {-1};
+        }
+        
+        int min = arr[0];
+        int[] result = new int[arr.length-1];
+        
+        for(int i=1; i<arr.length; i++){
+            if(min > arr[i]){
+                min = arr[i];
+            }
+        }
+        
+        
+        int index = 0;
+        for(int i=0; i<arr.length; i++){
+            if(min != arr[i]){
+                result[index++] = arr[i];
+            } 
+        }
+        
+        return result;
+    }
+}
