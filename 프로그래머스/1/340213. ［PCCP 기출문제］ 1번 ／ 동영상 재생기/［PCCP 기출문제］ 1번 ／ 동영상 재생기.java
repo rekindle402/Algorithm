8fd92b0d09d4217814b3336fad571bbd8f0807be
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-05-16 21:45:05 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 8.40 ms, Memory: 60.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -0,0 +1,76 @@
+class Solution {
+    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands){
+        
+        int startOpening = toSec(op_start);
+        int endOpening = toSec(op_end);
+            
+        if(isOpening(startOpening, endOpening, pos)) pos = toPos(endOpening);
+        
+        for(String command : commands){
+            pos = switch(command){
+                case "next" -> next(pos, video_len);
+                case "prev" -> prev(pos, video_len);
+                default -> "00:00";
+            };
+            if(isOpening(startOpening, endOpening, pos)) pos = toPos(endOpening);
+        }
+        
+        return pos;
+    }
+    
+    private boolean isOpening(int opStart,int opEnd, String pos){
+            int currentTime = toSec(pos);
+            
+            return opStart <= currentTime && currentTime <= opEnd;
+    }
+        
+    private String next(String pos, String video_len){
+        final int NEXT_TIME = 10;
+        
+        int nextSec = toSec(pos) + NEXT_TIME;
+        int endSec = toSec(video_len);
+        
+        return nextSec >= endSec ? video_len : toPos(nextSec);
+    }
+    
+    private String prev(String pos, String video_len){
+        final int PREV_TIME = 10;
+        final int START_SEC = 0;
+        
+        int prevSec = toSec(pos) - PREV_TIME;
+        
+        return prevSec <= START_SEC ? "00:00" : toPos(prevSec);
+    }
+    
+    private int toSec(String pos){
+        String[] time = pos.split(":");
+        
+        int sec = Integer.parseInt(time[0]) * 60 + Integer.parseInt(time[1]);
+        
+        return sec;
+    }
+    
+    private String toPos(int sec){
+        int minute = sec / 60;
+        String posMinute = "";
+        
+        int remainSec = sec % 60;
+        String posSec = "";
+        
+        // mm 형식으로 파싱
+        if(minute < 10){
+             posMinute = "0" + String.valueOf(minute);
+        } else {
+            posMinute = String.valueOf(minute);
+        }
+        
+        // ss 형식으로 파싱
+        if(remainSec < 10){
+            posSec = "0" + String.valueOf(remainSec);
+        } else {
+            posSec = String.valueOf(remainSec);
+        }
+        
+        return posMinute + ":" + posSec;
+    }
+}
