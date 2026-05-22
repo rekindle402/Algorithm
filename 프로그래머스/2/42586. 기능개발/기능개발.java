8f9731354b8573de5eee5e9b018386f46f714948
JaeHwan Jung <102033760+rekindle402@users.noreply.github.com>
2026-05-22 17:39:56 +0900

    [level 2] Title: 기능개발, Time: 0.21 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,20 +5,20 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        List<Integer> list = new ArrayList<>();
+        Queue<Integer> queue = new ArrayDeque<>(); // 기능 완성일을 담는 큐
+        List<Integer> list = new ArrayList<>(); // 결과 반환용 List
         
-        // 작업 완료 일수 계산
+        // 기능 완성일 계산
         for(int i=0; i<progresses.length; i++){
             int remainRate = 100 - progresses[i];
             int completeDays = (remainRate + speeds[i] - 1) / speeds[i];
             queue.offer(completeDays);
         }
         
-        int releaseDate = queue.poll(); // 이전 작업 배포 일
-        int count = 1; // 배포된 작업 개수
+        int releaseDate = queue.poll(); // 최 우선 기능 배포 일
+        int count = 1; // 배포된 기능의 개수
         
-        // 함께 배포되는 작업 개수 계산
+        // 배포되는 기능의 개수 계산
         while(!queue.isEmpty()){
             if(releaseDate >= queue.peek()){
                 count++;
@@ -29,17 +29,20 @@ class Solution {
                 releaseDate = queue.poll();
             }
         }
-        
         list.add(count);
+        
         
         return toIntArray(list);
     }
     
+    // Convert List to Array
     private int[] toIntArray(List<Integer> list){
         int[] answer = new int[list.size()];
+                               
         for(int i=0; i<list.size(); i++){
             answer[i] = list.get(i).intValue();
         }
+                               
         return answer;
     }
 }
